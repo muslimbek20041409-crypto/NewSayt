399271bc6de2ef6c5c6998c4ef0f312064e98b0b
muslimbek20041409-crypto <muslimbek20041409@gmail.com>
2026-04-29 17:09:25 +0500

    Update Result.java

--- src/main/java/com/example/testsaytproyekt/result/entity/Result.java
+++ src/main/java/com/example/testsaytproyekt/result/entity/Result.java
@@ -8,7 +8,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
