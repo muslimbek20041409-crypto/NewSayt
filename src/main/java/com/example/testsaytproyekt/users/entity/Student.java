c9219ab9cde3a82e14e93c82e24f50e83bf2cb83
muslimbek20041409-crypto <muslimbek20041409@gmail.com>
2026-04-29 17:09:44 +0500

    Update Student.java

--- src/main/java/com/example/testsaytproyekt/users/entity/Student.java
+++ src/main/java/com/example/testsaytproyekt/users/entity/Student.java
@@ -10,7 +10,7 @@ import lombok.*;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
-
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.List;
