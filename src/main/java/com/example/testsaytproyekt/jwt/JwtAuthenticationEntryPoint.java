78ba16cad77f53528f67e7192e1148209112842d
MUSLIM <muslimbek20041409@gmail.com>
2026-04-29 15:53:50 +0500

    jwt

--- src/main/java/com/example/testsaytproyekt/jwt/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/example/testsaytproyekt/jwt/JwtAuthenticationEntryPoint.java
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import lombok.NonNull;
+import org.springframework.lang.NonNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.core.AuthenticationException;
